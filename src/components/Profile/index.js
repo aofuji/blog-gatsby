23955ecb46c977a676b044a3ea633e443443f05a
Andre Ofuji <30282153+aofuji@users.noreply.github.com>
2023-09-13 21:30:18 -0300

    blog

---
+++ src/components/Profile/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby';
+import Avatar from '../Avatar/index'
+import * as S from './styled'
+import getThemeColor from '../../utils/getThemeColor'
+
+const Profile = () => {
+
+    const {
+        site: {
+            siteMetadata: { title, position, description },
+        },
+    } = useStaticQuery(graphql`
+query MySiteMetaData {
+    site {
+        siteMetadata {
+        title
+        position
+        description
+        author
+        }
+      }
+    }
+`)
+
+    return (
+        <S.ProfileWrapper>
+            <S.ProfileLink  to="/"  cover direction="left" bg={getThemeColor()} duration={0.6}>
+                <Avatar></Avatar>
+                <S.ProfileAuthor>
+                    {title}
+                    <S.ProfilePosition>{position}</S.ProfilePosition>
+                </S.ProfileAuthor>
+            </S.ProfileLink>
+            <S.ProfileDescription> {description} </S.ProfileDescription>
+            
+        </S.ProfileWrapper>
+    )
+
+}
+
+
+
+export default Profile
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,26 @@
+// const handle = 'aofuji'
+
+const links = [
+    {
+        label: 'Github',
+        url: 'https://github.com/aofuji'
+    },
+    {
+        label: "Twitter",
+        url: `https://twitter.com/aofuji`,
+    },
+    {
+        label: "Youtube",
+        url: `https://www.youtube.com/aofuji`,
+    },
+    {
+        label: "Instagram",
+        url: `https://www.instagram.com/aofuji`,
+    },
+    {
+        label: "Unsplash",
+        url: `https://unsplash.com/aofuji`,
+    },
+]
+
+export default links
